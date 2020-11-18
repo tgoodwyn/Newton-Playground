183aa83d0d4abf58d02aabcf09a561a3c84115c0
Tyler Goodwyn <tgoodwyn@gmail.com>
2020-11-17 22:49:03 -0500

    "changed main pkg"

--- src/main/Main.java
+++ src/main/Main.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package test;
+package main;
 
 import view.GameScreen;
 import view.Window;
@@ -13,7 +13,7 @@ import view.Screen;
  *
  * @author team 2
  */
-public class Test {
+public class Main {
 
     /**
      * @param args the command line arguments
