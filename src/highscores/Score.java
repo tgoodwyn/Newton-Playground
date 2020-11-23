f2be44c42c3b17f7d9b4e317ad1d2794dc960c3e
Justin <jbh0027@uah.edu>
2020-11-23 12:47:43 -0600

    Added Highscore Package

---
+++ src/highscores/Score.java
@@ -0,0 +1,30 @@
+package highscores;
+
+import java.io.Serializable;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author justi
+ */
+public class Score implements Serializable{
+     private int score;
+    private String name;
+
+    public int getScore() {
+        return score;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Score(String naam, int score) {
+        this.score = score;
+        this.name = name;
+    }
+}
