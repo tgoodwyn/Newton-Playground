b7d49d12c8a061efae20d56039e350716ec64e8d
Tyler Goodwyn <tgoodwyn@gmail.com>
2020-11-23 11:34:20 -0500

    deleted text field from example

--- src/cards/Third.java
+++ src/cards/Third.java
@@ -38,8 +38,6 @@ public class Third extends Card {
         testButtonCard = new javax.swing.JButton(new NextAction("managed next",cm.getManager()));
         nameButton = new javax.swing.JButton();
         jLabel1 = new javax.swing.JLabel();
-        jScrollPane1 = new javax.swing.JScrollPane();
-        ancestorButtonTextDisplay = new javax.swing.JTextArea();
         NameField = new javax.swing.JTextField();
 
         setBackground(new java.awt.Color(255, 255, 102));
@@ -56,12 +54,6 @@ public class Third extends Card {
         jLabel1.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
         jLabel1.setText("This is the third card in the deck");
 
-        ancestorButtonTextDisplay.setColumns(20);
-        ancestorButtonTextDisplay.setLineWrap(true);
-        ancestorButtonTextDisplay.setRows(5);
-        ancestorButtonTextDisplay.setWrapStyleWord(true);
-        jScrollPane1.setViewportView(ancestorButtonTextDisplay);
-
         NameField.setText("Default");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
@@ -69,36 +61,32 @@ public class Third extends Card {
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(jLabel1)
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-            .addGroup(layout.createSequentialGroup()
-                .addGap(36, 36, 36)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(nameButton)
-                    .addComponent(testButtonCard)
-                    .addComponent(NameField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 55, Short.MAX_VALUE)
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 201, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(27, 27, 27))
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addGroup(layout.createSequentialGroup()
+                        .addContainerGap()
+                        .addComponent(jLabel1))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(36, 36, 36)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(nameButton)
+                                .addGap(54, 54, 54)
+                                .addComponent(NameField))
+                            .addComponent(testButtonCard))))
+                .addGap(27, 96, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
                 .addComponent(jLabel1)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(70, 70, 70)
-                        .addComponent(testButtonCard)
-                        .addGap(18, 18, 18)
-                        .addComponent(nameButton)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .addComponent(NameField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(27, 27, 27)
-                        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 157, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                .addContainerGap(83, Short.MAX_VALUE))
+                .addGap(70, 70, 70)
+                .addComponent(testButtonCard)
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(nameButton)
+                    .addComponent(NameField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap(133, Short.MAX_VALUE))
         );
     }// </editor-fold>//GEN-END:initComponents
 
@@ -120,9 +108,7 @@ public class Third extends Card {
     }//GEN-LAST:event_nameButtonActionPerformed
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JTextField NameField;
-    private javax.swing.JTextArea ancestorButtonTextDisplay;
     private javax.swing.JLabel jLabel1;
-    private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JButton nameButton;
     private javax.swing.JButton testButtonCard;
     // End of variables declaration//GEN-END:variables
